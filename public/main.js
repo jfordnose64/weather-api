e2047ee050d0b81f41e454932be219461f9e3f55
jackson ford <jfordnose64@gmail.com>
2019-08-13 22:26:16 -0400

    working example

--- public/main.js
+++ public/main.js
@@ -1,7 +1,24 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let city = document.querySelector('.input-field')
+
+const createProperSearch = () => {
+  let cityName = city.value
+  const url = 'http://api.openweathermap.org/data/2.5/weather'
+  let search = cityName
+  const apiKey = '32b3245d73de3274c4273429518d2502'
+  const newUrl = url + '?' + 'q=' + search + '&units=imperial' + '&appid=' + apiKey
+  return newUrl
+}
+const getWeather = () => {
+  fetch(createProperSearch())
+    .then(response => {
+      return response.json()
+    })
+    .then(city => {
+      console.log(city)
+      document.querySelector('.display-weather').textContent = city.weather[0].main
+      document.querySelector('.display-temp').textContent = city.main.temp
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+// document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.enter').addEventListener('click', getWeather)
---
+++ public/test.js
@@ -0,0 +1,40 @@
+@const main = async () => {
+  // want to call the API using fetch
+  // console.log the response
+  document.querySelector('.punchline').classList.add('hide')
+  document.querySelector('.setup').textContent = '...'
+  const response = await fetch(
+    'https://official-joke-api.appspot.com/random_joke'
+  )
+  console.log(response)
+  if (response.status === 200) {
+    const joke = await response.json()
+    console.log(joke)
+    document.querySelector('.setup').textContent = joke.setup
+    document.querySelector('.punchline').textContent = joke.punchline
+  }
+}
+document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.reveal-punchline').addEventListener('click', () => {
+  document.querySelector('.punchline').classList.remove('hide')
+})
+document.querySelector('.get-another').addEventListener('click', main)
+Collapse
+
+
+
+
+
+
+
+Message cohort - xv
+
+const getWeatherApiUrl = () => {
+  const query = searchField.value
+  const mainPart = 'http://api.openweathermap.org/data/2.5/weather?'
+  const searchQuery = `q=${query}&`
+  const apiKeyPart = `appid=${API_KEY}`
+  const url = `${mainPart}${searchQuery}${apiKeyPart}`
+
+  getData(url)
+}
